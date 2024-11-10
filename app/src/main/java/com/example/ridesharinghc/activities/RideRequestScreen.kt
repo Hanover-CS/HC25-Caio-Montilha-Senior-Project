eb7e0a72084f1ff79d54f3d63051169304e1f753
Caio Montilha <94456335+cmontilha@users.noreply.github.com>
2024-11-09 22:12:35 -0500

    Updated RideRequestScreen.kt comment for better description

--- app/src/main/java/com/example/ridesharinghc/activities/RideRequestScreen.kt
+++ app/src/main/java/com/example/ridesharinghc/activities/RideRequestScreen.kt
@@ -6,6 +6,13 @@ import androidx.activity.compose.setContent
 import com.example.ridesharinghc.ui.theme.RideSharingHCTheme
 import com.example.ridesharinghc.composables.screens.RideRequestScreen.RideRequestScreenContent
 
+/**
+ * [RideRequestScreen] activity allows users to request a ride within the RideSharingHC app.
+ * It displays a map for selecting pickup and drop-off locations, and includes fields for
+ * the date, time, and any additional notes. Users can submit a ride request, which is saved
+ * to Firebase Firestore for viewing by other users.
+ * The activity sets up the UI theme and initializes the [RideRequestScreenContent] composable.
+ */
 class RideRequestScreen : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
