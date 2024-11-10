83c2a3f4a8fcaa1b42bdec6f97166626f1436750
Caio Montilha <94456335+cmontilha@users.noreply.github.com>
2024-11-09 22:12:55 -0500

    Updated OfferRideScreen.kt comment for better description

--- app/src/main/java/com/example/ridesharinghc/activities/OfferRideScreen.kt
+++ app/src/main/java/com/example/ridesharinghc/activities/OfferRideScreen.kt
@@ -8,6 +8,13 @@ import com.google.android.libraries.places.api.Places
 import com.example.ridesharinghc.composables.screens.OfferRideScreenContent
 import com.example.ridesharinghc.BuildConfig
 
+/**
+ * [OfferRideScreen] activity allows users to offer a ride within the RideSharingHC app.
+ * It displays a map for selecting a pickup location, and provides fields for the driver's name,
+ * pickup/drop-off location, date, time, and available seats.
+ * Once submitted, the ride offer is saved to Firebase Firestore.
+ * The activity initializes the Places API with the configured MAPS_KEY.
+ */
 class OfferRideScreen : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
