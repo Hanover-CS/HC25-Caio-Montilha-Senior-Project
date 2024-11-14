be848938ec859514c71625eacb688adafca50218
Caio Montilha <94456335+cmontilha@users.noreply.github.com>
2024-11-13 20:33:03 -0500

    Refactor: move OfferInputFields.kt to separate file for cleaning code

---
+++ app/src/main/java/com/example/ridesharinghc/composables/screens/OfferRideScreen/OfferInputFields.kt
@@ -0,0 +1,64 @@
+package com.example.ridesharinghc.composables.screens.OfferRideScreen
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.input.KeyboardType
+
+@Composable
+fun OfferInputFields(pickupLocation: MutableState<String>) {
+    val driverName = remember { mutableStateOf("") }
+    val date = remember { mutableStateOf("") }
+    val time = remember { mutableStateOf("") }
+    val seatsAvailable = remember { mutableStateOf("") }
+
+    TextField(
+        value = driverName.value,
+        onValueChange = { driverName.value = it },
+        label = { Text("Driver's Name") },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+    )
+    TextField(
+        value = pickupLocation.value,
+        onValueChange = { pickupLocation.value = it },
+        label = { Text("Pickup Location") },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+    )
+    TextField(
+        value = date.value,
+        onValueChange = { date.value = it },
+        label = { Text("Date (MM/DD/YYYY)") },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
+    TextField(
+        value = time.value,
+        onValueChange = { time.value = it },
+        label = { Text("Time") },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+    )
+    TextField(
+        value = seatsAvailable.value,
+        onValueChange = { seatsAvailable.value = it },
+        label = { Text("Seats Available") },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+    )
+}
--- app/src/main/java/com/example/ridesharinghc/composables/screens/OfferRideScreen/OfferRideScreenContent.kt
+++ app/src/main/java/com/example/ridesharinghc/composables/screens/OfferRideScreen/OfferRideScreenContent.kt
@@ -1,46 +1,24 @@
 package com.example.ridesharinghc.composables.screens
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.widget.Toast
-import androidx.activity.ComponentActivity
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import com.example.ridesharinghc.R
 import com.example.ridesharinghc.ui.theme.SoftBlue
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.LatLng
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.example.ridesharinghc.composables.screens.common.LocationSearchBar
-import com.google.maps.android.compose.CameraPositionState
 import com.example.ridesharinghc.composables.screens.common.checkLocationPermissionAndSetLocation
 import com.example.ridesharinghc.composables.screens.common.BackButton
 import com.example.ridesharinghc.composables.screens.common.MapSection
 import com.example.ridesharinghc.composables.screens.OfferRideScreen.OfferSubmitButton
+import com.example.ridesharinghc.composables.screens.OfferRideScreen.OfferInputFields
 
 @Composable
 fun OfferRideScreenContent(onBackClick: () -> Unit) {
@@ -82,47 +60,4 @@ fun OfferRideScreenContent(onBackClick: () -> Unit) {
             pickupLocation = pickupLocation.value
         )
     }
-}
-
-
-@Composable
-fun OfferInputFields(pickupLocation: MutableState<String>) {
-    val driverName = remember { mutableStateOf("") }
-    val date = remember { mutableStateOf("") }
-    val time = remember { mutableStateOf("") }
-    val seatsAvailable = remember { mutableStateOf("") }
-
-    TextField(
-        value = driverName.value,
-        onValueChange = { driverName.value = it },
-        label = { Text("Driver's Name") },
-        modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp)
-    )
-    TextField(
-        value = pickupLocation.value,
-        onValueChange = { pickupLocation.value = it },
-        label = { Text("Pickup Location") },
-        modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp)
-    )
-    TextField(
-        value = date.value,
-        onValueChange = { date.value = it },
-        label = { Text("Date (MM/DD/YYYY)") },
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 8.dp),
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-    )
-    TextField(
-        value = time.value,
-        onValueChange = { time.value = it },
-        label = { Text("Time") },
-        modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp)
-    )
-    TextField(
-        value = seatsAvailable.value,
-        onValueChange = { seatsAvailable.value = it },
-        label = { Text("Seats Available") },
-        modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp)
-    )
 }
